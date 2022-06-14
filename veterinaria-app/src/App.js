2421321f535f825eead765effbf2a99b46f0b457
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-06-14 20:24:08 -0300

    importé el Home en la ruta

--- veterinaria-app/src/App.js
+++ veterinaria-app/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Cards from './Components/Cards/Cards';
 import Pacientes from './Components/Pacientes/Pacientes';
+import Home from './Pages/Home';
 
 function App() {
   return (
