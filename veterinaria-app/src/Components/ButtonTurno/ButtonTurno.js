ab178a48bf7e0f8f3a76d585ecb354dbbdaa7708
nicolascianci <martincianci@gmail.com>
2022-06-15 22:17:37 -0300

    boton para solicitar turno

---
+++ veterinaria-app/src/Components/ButtonTurno/ButtonTurno.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ButtonTurno.css';
+
+const ButtonTurno = () => {
+    return (
+        <button type="button" class="boton-turno btn btn-primary btn-lg col-6">
+                <img className ="mx-3" src="./mascota.png" alt="">Pedir Turno</img>
+        </button>
+      );
+};
+
+export default ButtonTurno;
