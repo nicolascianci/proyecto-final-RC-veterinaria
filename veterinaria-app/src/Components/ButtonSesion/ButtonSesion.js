977e688220d8837146d508f75166abf97b46233f
nicolascianci <martincianci@gmail.com>
2022-06-15 22:31:54 -0300

    boton de inicio de sesion

---
+++ veterinaria-app/src/Components/ButtonSesion/ButtonSesion.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ButtonSesion.css';
+
+const ButtonSesion = () => {
+    return (
+        <button type="button" class="boton-sesion btn btn-primary btn-lg col-6">
+                <img class="mx-3" src="./caminando-con-perro.png" alt=""> Inicio de Sesion</img>
+        </button>
+      );
+};
+
+export default ButtonSesion;
