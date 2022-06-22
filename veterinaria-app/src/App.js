7d48f1a771e8fbb19a4edcfe24502a1d6aa6d447
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-06-22 19:30:01 -0300

    eliminar este pull request

--- veterinaria-app/src/App.js
+++ veterinaria-app/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+// import ButtonSesion from './Components/ButtonSesion/ButtonSesion';
+// import ButtonTurno from './Components/ButtonTurno/ButtonTurno';
 import Cards from './Components/Cards/Cards';
 import Carousel from './Components/Carousel/Carousel';
 import Pacientes from './Components/Pacientes/Pacientes';
