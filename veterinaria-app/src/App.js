d29a619826c0147fdfd04ffb002ca54c85ad51eb
Nicolas Gonzalez <ngonzalez@dminc.com>
2022-06-22 19:57:42 -0300

    update

--- veterinaria-app/src/App.js
+++ veterinaria-app/src/App.js
@@ -2,30 +2,31 @@ import React from 'react';
 import Cards from './Components/Cards/Cards';
 import Carousel from './Components/Carousel/Carousel';
 import Pacientes from './Components/Pacientes/Pacientes';
-// import {Routes, Route} from 'react-router-dom';
-// import Home from './Pages/Home';
-// import Error from './Pages/Error';
-// import Nosotros from './Pages/Nosotros';
-// import Servicios from './Pages/Servicios';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Error from './Pages/Error';
+import Nosotros from './Pages/Nosotros';
+import Servicios from './Pages/Servicios';
 
 function App() {
   return (
     <>
-      <div>
-        {/* <ButtonSesion />
-        <ButtonTurno /> */}
+      {/* <div>
+        <ButtonSesion />
+        <ButtonTurno />
         <Carousel />
         <Cards />
         <Pacientes />
-      </div>
+      </div> */}
 
-
-       {/* <Routes>
+      <BrowserRouter>
+       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='*' element={<Error />} />
         <Route path='/nosotros' element={<Nosotros />} />
         <Route path='/servicios' element={<Servicios />} />
-       </Routes> */}
+       </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- veterinaria-app/src/Pages/Home.js
+++ veterinaria-app/src/Pages/Home.js
@@ -1,17 +1,17 @@
 import React from 'react';
 // // import ButtonSesion from '../Components/ButtonSesion/ButtonSesion';
 // // import ButtonTurno from '../Components/ButtonTurno/ButtonTurno';
-// import Cards from '../Components/Cards/Cards';
-// import Pacientes from '../Components/Pacientes/Pacientes';
+import Cards from '../Components/Cards/Cards';
+import Pacientes from '../Components/Pacientes/Pacientes';
 
 const Home = () => {
   return (
-    // <div>
-    //   {/* <ButtonSesion />
-    //   <ButtonTurno /> */}
-    //   <Cards />
-    //   <Pacientes />
-    // </div>
+    <div>
+      {/* <ButtonSesion />
+      <ButtonTurno /> */}
+      <Cards />
+      <Pacientes />
+    </div>
   );
 };
 
