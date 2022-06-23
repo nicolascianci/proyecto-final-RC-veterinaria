227fd56db912c1f31863fff11e98d3f6d0d14439
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-06-23 19:39:22 -0300

    subí imágenes y configuré la información de los modales

--- src/Pages/Servicios.js
+++ src/Pages/Servicios.js
@@ -22,7 +22,7 @@ const Servicios = () => {
   <section class="row d-flex justify-content-center m-5">
 
     <div class="card w-25 border-1 rounded-0 p-0 m-0">
-      <img src="https://picsum.photos/650/400/?random=1" class="card-img-top rounded-0" alt="..." />
+      <img src="https://images.pexels.com/photos/6235118/pexels-photo-6235118.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="card-img-top rounded-0" alt="..." />
       <div class="card-body">
         <p class="card-text">Medicina Preventiva</p>
         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal1"> Ver más </button>
@@ -30,7 +30,7 @@ const Servicios = () => {
     </div>
 
     <div class="card w-25 border-1 rounded-0 p-0 m-0">
-      <img src="https://picsum.photos/650/400/?random=2" class="card-img-top rounded-0" alt="..." />
+      <img src="https://images.pexels.com/photos/6816869/pexels-photo-6816869.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="card-img-top rounded-0" alt="..." />
       <div class="card-body">
         <p class="card-text">Análisis de Laboratorio</p>
         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal2"> Ver más </button>
@@ -38,7 +38,7 @@ const Servicios = () => {
     </div>
 
     <div class="card w-25 border-1 rounded-0 p-0 m-0">
-      <img src="https://picsum.photos/650/400/?random=3" class="card-img-top rounded-0" alt="..." />
+      <img src="https://images.pexels.com/photos/8413323/pexels-photo-8413323.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="card-img-top rounded-0" alt="..." />
       <div class="card-body">
         <p class="card-text">Radiología Digital</p>
         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal3"> Ver más </button>
@@ -46,7 +46,7 @@ const Servicios = () => {
     </div>
 
     <div class="card w-25 border-1 rounded-0 p-0 m-0">
-      <img src="https://picsum.photos/650/400/?random=4" class="card-img-top rounded-0" alt="..." />
+      <img src="https://images.pexels.com/photos/6234610/pexels-photo-6234610.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="card-img-top rounded-0" alt="..." />
       <div class="card-body">
         <p class="card-text">Ecografía</p>
         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal4"> Ver más </button>
@@ -54,7 +54,7 @@ const Servicios = () => {
     </div>
 
     <div class="card w-25 border-1 rounded-0 p-0 m-0">
-      <img src="https://picsum.photos/650/400/?random=5" class="card-img-top rounded-0" alt="..." />
+      <img src="https://images.pexels.com/photos/6816858/pexels-photo-6816858.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="card-img-top rounded-0" alt="..." />
       <div class="card-body">
         <p class="card-text">Peluquería</p>
         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal5"> Ver más </button>
@@ -62,7 +62,7 @@ const Servicios = () => {
     </div>
 
     <div class="card w-25 border-1 rounded-0 p-0 m-0">
-      <img src="https://picsum.photos/650/400/?random=6" class="card-img-top rounded-0" alt="..." />
+      <img src="https://images.pexels.com/photos/1350593/pexels-photo-1350593.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="card-img-top rounded-0" alt="..." />
       <div class="card-body">
         <p class="card-text">Cirugía</p>
         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal6"> Ver más </button>
@@ -70,7 +70,7 @@ const Servicios = () => {
     </div>
 
     <div class="card w-25 border-1 rounded-0 p-0 m-0">
-      <img src="https://picsum.photos/650/400/?random=7" class="card-img-top rounded-0" alt="..." />
+      <img src="https://images.pexels.com/photos/7470752/pexels-photo-7470752.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="card-img-top rounded-0" alt="..." />
       <div class="card-body">
         <p class="card-text">Dermatología</p>
         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal7"> Ver más </button>
@@ -78,7 +78,7 @@ const Servicios = () => {
     </div>
 
     <div class="card w-25 border-1 rounded-0 p-0 m-0">
-      <img src="https://picsum.photos/650/400/?random=8" class="card-img-top rounded-0" alt="..." />
+      <img src="https://images.pexels.com/photos/6235240/pexels-photo-6235240.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="card-img-top rounded-0" alt="..." />
       <div class="card-body">
         <p class="card-text">Oftalmología</p>
         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal8"> Ver más </button>
@@ -94,12 +94,9 @@ const Servicios = () => {
         <h5 class="modal-title" id="exampleModalLabel">Medicina preventiva</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
+      <img src="https://images.pexels.com/photos/6235118/pexels-photo-6235118.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
       <div class="modal-body">
-        <p>Como prevenir es mejor que curar, te daremos los mejores consejos para el cuidado de tu mascota. Ven a informarte acerca de los planes vacunales y cómo desparasitar a tus animales.
-
-          Recibe información acerca de cómo educar a los cachorritos o qué alimentación darles. Hacemos planes de prevención para animales que van a viajar al extranjero, geriátricos o que sufren determinadas patologías.
-          
-          Realizamos chequeos periódicos de salud, análisis de leishmania (enfermedad del mosquito), filaria (gusano del corazón), controles de peso y mucho más. Acércate y pregunta.</p>
+        <p>Como prevenir es mejor que curar, te daremos los mejores consejos para el cuidado de tu mascota. Ven a informarte acerca de los planes vacunales, cómo desparasitar a tus animales, cómo educar a los cachorritos o qué alimentación darles. Hacemos planes de prevención para animales que van a viajar al extranjero, geriátricos o que sufren determinadas patologías. Realizamos chequeos periódicos de salud, análisis de leishmania (enfermedad del mosquito), filaria (gusano del corazón), controles de peso y mucho más.</p>
       </div>
      </div>
     </div>
@@ -109,11 +106,12 @@ const Servicios = () => {
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Medicina preventiva</h5>
+          <h5 class="modal-title" id="exampleModalLabel">Análisis de Laboratorio</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
+        <img src="https://images.pexels.com/photos/6816869/pexels-photo-6816869.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
         <div class="modal-body">
-          <p>Análisis de laboratorio</p>
+          <p>Nuestro Centro cuenta con equipamiento para hacer <strong>analíticas in situ </strong> . De esta manera se aceleran los procedimientos y podemos emitir un diagnóstico e implementar el tratamiento adecuado de manera rápida y efectiva.</p>
         </div>
        </div>
       </div>
@@ -123,11 +121,12 @@ const Servicios = () => {
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Medicina preventiva</h5>
+            <h5 class="modal-title" id="exampleModalLabel">Radiolog´pia Digital</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
+          <img src="https://images.pexels.com/photos/8413323/pexels-photo-8413323.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
           <div class="modal-body">
-            <p>Radiología digital</p>
+            <p>La radiología ofrece información sobre el estado de tejidos blandos y de huesos y articulaciones. Utilizamos la <strong>técnica digital</strong> que permite obtener una imagen mucho más clara. También facilitamos la difusión y utilización de las imágenes, puesto que se almacenan como archivos de imagen fácilmente compartibles con el cliente y/o veterinarios remitentes.</p>
           </div>
          </div>
         </div>
@@ -137,11 +136,12 @@ const Servicios = () => {
         <div class="modal-dialog">
           <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Medicina preventiva</h5>
+              <h5 class="modal-title" id="exampleModalLabel">Ecografía</h5>
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
+            <img src="https://images.pexels.com/photos/6234610/pexels-photo-6234610.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
             <div class="modal-body">
-              <p>Ecografía digital</p>
+              <p>La ecografía ofrece información sobre el estado de los órganos internos de manera no invasiva, inmediata y precisa. Además, es una herramienta muy útil en la toma de muestras y punciones, al hacerlas ecoguiadas.</p>
             </div>
            </div>
           </div>
@@ -151,11 +151,12 @@ const Servicios = () => {
           <div class="modal-dialog">
             <div class="modal-content">
               <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Medicina preventiva</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Peluquería</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
+              <img src="https://images.pexels.com/photos/6816858/pexels-photo-6816858.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
               <div class="modal-body">
-                <p>Peluquería</p>
+                <p>Realizamos cortes comerciales y con tijera. Cada animal es bañado con el champú y acondicionador más adecuado para su tipo de piel y pelo. Además, cortamos las uñas, limpiamos los oídos y vacíamos los sacos anales. Procuramos también tratamientos de champuterapia y corte terapéutico a los animales con problemas dermatológicos.</p>
               </div>
              </div>
             </div>
@@ -168,8 +169,9 @@ const Servicios = () => {
                   <h5 class="modal-title" id="exampleModalLabel">Medicina preventiva</h5>
                   <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
+                <img src="https://images.pexels.com/photos/1350593/pexels-photo-1350593.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
                 <div class="modal-body">
-                  <p>Cirugía</p>
+                  <p>Hacemos cirugías de esterilización, cirugía gastrointestinal, de hígado y sistema biliar, de vejiga, uréteres y riñones, adrenales, bazo y páncreas.Reparamos hernias abdominales, inguinales y perineales. Hacemos profilaxis dental, extracción de piezas dentarias, plastias cutáneas y cierre de heridas traumáticas.También realizamos cirugías con <strong>láser de CO2</strong>. Su aplicación ofrece importantes ventajas en cuanto a acortamiento del procedimiento quirúrgico, cicatrización y recuperación del paciente.</p>
                 </div>
                </div>
               </div>
@@ -179,11 +181,12 @@ const Servicios = () => {
               <div class="modal-dialog">
                 <div class="modal-content">
                   <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Medicina preventiva</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Dermatología</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
+                  <img src="https://images.pexels.com/photos/7470752/pexels-photo-7470752.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
                   <div class="modal-body">
-                    <p>Dermatología</p>
+                    <p>En Veterinaria Huellitas entendemos que un buen diagnóstico es primordial y para ello realizamos citologías, tricogramas, pruebas de alergia y cualquier otro tipo de pruebas para detectar el problema.</p>
                   </div>
                  </div>
                 </div>
@@ -193,11 +196,12 @@ const Servicios = () => {
                 <div class="modal-dialog">
                   <div class="modal-content">
                     <div class="modal-header">
-                      <h5 class="modal-title" id="exampleModalLabel">Medicina preventiva</h5>
+                      <h5 class="modal-title" id="exampleModalLabel">Oftalmología</h5>
                       <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
+                    <img src="https://images.pexels.com/photos/6235240/pexels-photo-6235240.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
                     <div class="modal-body">
-                      <p>Oftalmología</p>
+                      <p>Tratamos enfermedades como la queratoconjuntivitis seca, úlceras corneales indolentes o las anomalías en los párpados, patologías comunes en determinadas razas.</p>
                     </div>
                    </div>
                   </div>
