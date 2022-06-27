9971fd0d5eb8ad4b41a6397d3a6441407448ce4f
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-06-27 06:32:41 -0300

    cambié estilos de los botones para aunar con el diseño del front

--- src/Components/ButtonSesion/ButtonSesion.js
+++ src/Components/ButtonSesion/ButtonSesion.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './ButtonSesion.css';
 import img from '../assests/caminando-con-perro.png';
+import { Link } from 'react-router-dom';
 
 const ButtonSesion = () => {
     return (
-      <a href="/login">
-        <button type="button" className="boton-sesion btn btn-primary btn-lg col-6">
+      <Link to="/login">
+        <button type="button" className="boton-sesion btn btn-primary btn-lg col-6 py-4">
                 <img className="mx-3" src={img} alt="" /> Ingresar
-        </button></a>
+        </button></Link>
       );
 };
 
