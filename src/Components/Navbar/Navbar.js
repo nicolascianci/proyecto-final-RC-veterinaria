31fd35f4561f8ae0487bd774cd878dd0417e08ad
Nicolas <juannicolasgomez3@gmail.com>
2022-06-30 21:48:46 -0300

    arreglando menú hamburguesa

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -11,13 +11,13 @@ const Navbar = () => {
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="navbar-collapse justify-content-center" id="navbarNav">
-                    <ul class="navbar-nav">
+                <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="#">Nosotros</a>
+                            <a class="nav-link " aria-current="page" href="#">NOSOTROS</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Servicios</a>
+                            <a class="nav-link" href="#">SERVICIOS</a>
                         </li>
                     </ul>
                 </div>
