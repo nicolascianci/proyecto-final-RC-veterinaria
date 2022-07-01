0d1f3c19a31608bcbbb7314fc84178a1a0c3b78c
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-07-01 14:59:11 -0300

    corregí errores con router-dom en links

--- src/Components/ButtonConsulta/ButtonConsulta.js
+++ src/Components/ButtonConsulta/ButtonConsulta.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './ButtonConsulta.css';
-import img from '../assests/mascota.png'
+import img from '../assests/mascota.png';
+import { Link } from 'react-router-dom';
 
 const ButtonConsulta = () => {
     return (
-      <a href="/consulta">
+      <Link to="/consulta">
         <button type="button" className="boton-turno btn btn-primary btn-lg col-6 py-4">
                 <img className="mx-3" src={img} alt="" /> Consultar
         </button>
-        </a>
+        </Link>
       );
 };
 
