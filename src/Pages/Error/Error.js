ab072cf3fcf02f98ac2930d960e672e8df6143b8
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-07-04 17:47:07 -0300

    pagina de error lista

--- src/Pages/Error/Error.js
+++ src/Pages/Error/Error.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import '../Error/Error.css';
+import { Link } from 'react-router-dom';
 
 const Error = () => {
   return (
-    <div>
-      <h1>Error page</h1>
+    <div className="errorpage d-flex align-items-center justify-content-center">
+      <div className="text-error">
+      <h1 className="text-light text-center w-100 p-5"><strong>¡UPS!</strong><br />PARECE QUE LA PÁGINA QUE BUSCAS NO EXISTE</h1>
+      </div>
+        <Link to="/">
+      <button className="btn-error btn rounded-0 fs-5">Volver a Página Principal</button>
+      </Link>
     </div>
   );
 };
