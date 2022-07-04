1eaacb4c14c3924e7fe211d6ee204851efeb69cf
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-07-03 22:34:53 -0300

    agregué botón hamburguesa en responsive y corregí estilos; navbar listo

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,32 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
-import ButtonSession from '../ButtonSesion/ButtonSesion';
-import ButtonConsulta from '../ButtonConsulta/ButtonConsulta';
 import img from '../assests/logo.png';
+import ButtonConsulta from "../ButtonConsulta/ButtonConsulta";
+import ButtonSesion from "../ButtonSesion/ButtonSesion";
 
 const Navbar = () => {
   return (
-        <header class="navbar-style sticky-top d-lg-flex flex-lg-column">
-            <nav class="navbar navbar-expand-lg d-flex justify-content-between">
-                <div class="navbar-logo">
-                <Link class="navbar-brand text-light" to="/"><img className="img-navbar img" src={img} alt="" />
-                    LA HUELLITA</Link>
-                </div>
-                    <ul class="navbar-nav mb-2 mb-lg-0">
-                    <li class="nav-item">
-                            <Link class="nav-link text-light" aria-current="page" to="/nosotros">Nosotros</Link>
-                    </li>
-                    <li class="nav-item">
-                            <Link class="nav-link text-light" aria-current="page" to="/servicios">Servicios</Link>
-                    </li>
-                    </ul>
-                <div class="navbar-button p-0">
-                    <ButtonConsulta />
-                    <ButtonSession />
-                </div>
-            </nav>
-        </header>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light p-0">
+  <div className="container-fluid px-0">
+    <Link to="/">
+    <img src={img} className="logo-navbar" alt="logo" />
+    </Link>
+    <Link className="navbar-brand text-light fs-4 pe-5" to="/">La Huellita</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex text-center">
+          <Link className="nav-link text-light fs-5" aria-current="page" to="/nosotros">Nosotros</Link>
+          <Link className="nav-link text-light fs-5" aria-current="page" to="/servicios">Servicios</Link>
+      </ul>
+      <div className="d-flex justify-content-center">
+        <div className="botones-navbar">
+      <ButtonConsulta />
+      <ButtonSesion />
+      </div>
+      </div>
+    </div>
+  </div>
+</nav>
     );
 }
 
